49d624b9c5096303cd2a8b481a577c65265a461b
Israel <isra.ixoye@gmail.com>
2020-05-19 14:56:25 -0500

    Acciones con el mouse

--- mouse.asm
+++ mouse.asm
@@ -2,12 +2,12 @@ PRINT MACRO mensaje         ;Macro para imprimir el mensaje que recibe como para
     MOV AH,09H              
     LEA DX, mensaje
     INT 21H
-ENDM
+ENDM                        ; Fin de macro imprimir mensaje
 READ MACRO                  ;Macro para leer un caracter del teclado
     MOV AH,01H
     INT 21H
     SUB AL, 30H
-ENDM
+ENDM                        ; Fin de macro leer un caracter
 clear MACRO                 ;Macro para limpiar la pantalla de la consola 
     ;Establecer modo video (Texto)
     MOV AH,00H
@@ -19,7 +19,7 @@ clear MACRO                 ;Macro para limpiar la pantalla de la consola
     MOV DH,0
     MOV DL,20
     MOV BX,1  
-ENDM
+ENDM                        ; Fin de macro limpiar pantalla 
 
 sstack segment stack            ;Start del segmento de pila
     db 32 DUP('stack--') 
@@ -31,19 +31,15 @@ sdata segment                   ;Start del segmento de datos
     LETRE4 DB 'SE MUESTRA EL MOUSE $'
     LETRE5 DB 'PRIMERO MUESTRE EL MOUSE $'
     LETRE6 DB 'SE OCULTA EL MOUSE $'
-    LETRE7 DB 'PRESIONE UN BOTON DEL MOUSE'
-    LETRE8 DB 'BOTON IZQUIERDO $'
-    LETRE9 DB 'BOTON DERECHO $'
     
     FLAG0 DB 1         ;Bandera de si esta o no instalado el mouse
     FLAG1 DB 1         ;Bandera si se muestra o no el mouse
     FLAG2 DB 1         ;Bandera si se oculta o no el mouse
 
-    msgMenu DB '       MOUSE',13,10                       ;Mensaje de Menú de las opciones de la calculadora
+    msgMenu DB '       MOUSE',13,10                       ;Mensaje de Menú de las opciones del mouse
         DB '    1.- ACTIVAR   (00H)',13,10
         DB '    2.- MOSTRAR   (01H)',13,10
         DB '    3.- OCULTAR   (02H)',13,10
-        DB '    4.- DETECTAR BOTON (03H)',13,10
         DB '    8.- EXIT',13,10,13,10
         DB '    SELECCIONE UNA OPCI?N    $'
 sdata ends                      ;End del segmento de Datos
@@ -52,7 +48,7 @@ scode SEGMENT 'CODE'            ;Start del segmento de Código
         Princ PROC FAR          ;Start del procedimiento principal
         MOV AX, sdata
         MOV ds, AX	
-  
+        
         SUB AX, AX          
         MENU:
         clear
@@ -64,11 +60,9 @@ scode SEGMENT 'CODE'            ;Start del segmento de Código
         CMP AL,2
             JE MOSTRAR
         CMP AL,3
-            JE OCULTAR
-        CMP AL,4
-            JE DETECTAR
+            JE OCULTAR1
         CMP AL,8
-            JE SALIR
+            JE SALIR1
 
         ACTIVAR:
             MOV AX, 0000H           ; Función que inicializa el mouse
@@ -96,13 +90,18 @@ scode SEGMENT 'CODE'            ;Start del segmento de Código
                 READ
                 ;JMP SALIR
         JMP MENU
-
+        OCULTAR1:
+            JMP OCULTAR
+        SALIR1:
+            JMP SALIR
         MOSTRAR:
             CMP FLAG0,0
             JNE MENS
             MOV FLAG1,0
-            MOV AX,01H
-            INT 33H
+
+            MOV AX,01H              ; Función para mostrar el mouse
+            INT 33H                 ; Interrupcion del mouse
+            
             clear
             PRINT LETRE4
             READ
@@ -118,8 +117,8 @@ scode SEGMENT 'CODE'            ;Start del segmento de Código
             CMP FLAG1,0
             JNE MENS1
                 MOV FLAG2,0
-                MOV AX,02H
-                INT 33H
+                MOV AX,02H      ; Función para ocultar el mouse
+                INT 33H         ; Interrupción del mouse
                 clear
                 PRINT LETRE6
                 READ
@@ -130,10 +129,7 @@ scode SEGMENT 'CODE'            ;Start del segmento de Código
                 PRINT LETRE5
                 READ 
         JMP MENU
-        DETECTAR:
-            CMP FLAG1,0
-            JNE MENS1
-        JMP MENU
+       
         SALIR:
             MOV AX,4C00H 
             INT 21H
